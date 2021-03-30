54041b4e0509195d1961bd3a8315d28c3e16c4fc
giulio <giulio09@marcov.it>
2021-03-30 14:09:24 +0000

    Landing page video

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,28 +2,33 @@ import { RouteComponentProps } from 'react-router-dom';
 import {
   IonPage,
   IonButton,
-  IonHeader,
-  IonToolbar,
   IonContent,
-  IonTitle,
+  IonText
 } from '@ionic/react';
+
+import './HomePage.css';
+
+import background_video from "../images/background_video.mp4"
 
 const HomePage: React.FC<RouteComponentProps> = (props) => {
     return <IonPage>
-            <IonHeader>
-            <IonToolbar>
-                <IonTitle>Home</IonTitle>
-            </IonToolbar>
-            </IonHeader>
             <IonContent className="ion-padding">
+                <video id="videobackground" className='videobackground' playsInline loop muted autoPlay>
+                    <source src={background_video} type='video/mp4' />
+                </video>
                 <IonButton
-                    size="large"
+                    id="bottoneapp"
                     onClick={e => {
                         e.preventDefault();
                         props.history.push('/tabs');
                     }}>
                     App
                 </IonButton>
+                <IonText id="titolo">
+                    <h1 style={{fontSize: "100px", display: "table-cell", verticalAlign: "middle"}}>
+                        Titolo progetto
+                    </h1>
+                </IonText>
             </IonContent>
         </IonPage>
 }
